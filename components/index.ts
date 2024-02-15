cff53a434d578c166006b848787e0ec7e3c6128b
Calvin Teixeira <calvingsx@gmail.com>
2024-02-14 22:57:59 -0300

    Adicionando animação na seção de tecnologias

--- components/index.ts
+++ components/index.ts
@@ -1,5 +1,6 @@
 import Header from "./structure/Header";
 import TechAccordion from "./content/TechAccordion";
+import TecnologiesList from "./content/TecnologiesList";
 import { Button } from "./ui/button";
 
-export { Header, Button, TechAccordion }
+export { Header, Button, TechAccordion, TecnologiesList }
